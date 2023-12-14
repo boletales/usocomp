0cf0f9636cca29c594c97bb2188c2e433b35de8f
boletales <30789006+boletales@users.noreply.github.com>
2023-12-14 06:41:17 +0000

    WIP: start writing FromFunc

--- src/FuncLang/Def.hs
+++ src/FuncLang/Def.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE GADTs #-}
+
 module FuncLang.Def where
 
 import Data.Vector as V
+import Data.Map as M
 import Data.Text as T
 import Data.Kind
 
@@ -12,11 +15,18 @@ data FLType =
     deriving (Show, Eq)
 
 newtype FLVar (tag :: Type) (t :: FLType) = FLVar tag deriving (Show, Eq)
+data UntypedFLVar (tag :: Type) = UFLVar tag FLType
+data FLTopLevelVar (tag :: Type) where
+  FLTopLevelVar :: FLVar tag t -> FLExp tag t -> FLTopLevelVar tag
 
-data FLExp (t :: FLType) where
-  FLEValI   :: Int -> FLExp 'FLTInt
-  FLEValB   :: Int -> FLExp 'FLTBool
-  FLEVar    :: FLVar tag t -> FLExp t
-  FLELambda :: FLVar tag t -> FLExp t2 -> FLExp ('FLTLambda t1 t2)
-  FLEApp    :: FLExp ('FLTLambda t1 t2) -> FLExp t1 -> FLExp t2
-  FLELet    :: FLVar tag t1 -> FLExp t1 -> FLExp t2 -> FLExp t2
+data FLExp (tag :: Type) (t :: FLType) where
+  FLEValI   :: forall   tag      . Int -> FLExp tag 'FLTInt
+  FLEValB   :: forall   tag      . Int -> FLExp tag 'FLTBool
+  FLEVar    :: forall t tag      . FLVar tag t -> FLExp tag t
+  FLELambda :: forall t tag t1 t2. FLVar tag t -> FLExp tag t2  -> FLExp tag ('FLTLambda t1 t2)
+  FLEApp    :: forall t tag t1 t2. FLExp tag ('FLTLambda t1 t2) -> FLExp tag t1 -> FLExp tag t2
+  FLELet    :: forall t tag t1 t2. FLVar tag t1 -> FLExp tag t2 -> FLExp tag t2
+
+data FLProgram (tag :: Type) = FLProgram {
+      flpTopLevelVars :: M.Map tag (FLTopLevelVar tag)
+  }
--- src/SimpleLang/FromFuncLang.hs
+++ src/SimpleLang/FromFuncLang.hs
@@ -2,3 +2,10 @@ module SimpleLang.FromFuncLang where
 
 import SimpleLang.Def
 
+{-
+コンパイル手順：
+0. 識別子を一意なものに変換する
+1. すべての関数をトップレベルに持ち上げる
+2. 不完全な適用はクロージャ（SLExp）に、完全な適用はSLEPushCallなりslmTailCallReturnなりにする
+-}
+
