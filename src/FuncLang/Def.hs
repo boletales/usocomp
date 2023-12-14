26f7fedbb4de42d3f0854f4ff6bc275efce1aee4
boletales <30789006+boletales@users.noreply.github.com>
2023-12-15 04:19:08 +0900

    implement Identifier renamer

--- src/FuncLang/Def.hs
+++ src/FuncLang/Def.hs
@@ -16,17 +16,17 @@ data FLType =
 
 newtype FLVar (tag :: Type) (t :: FLType) = FLVar tag deriving (Show, Eq)
 data UntypedFLVar (tag :: Type) = UFLVar tag FLType
-data FLTopLevelVar (tag :: Type) where
-  FLTopLevelVar :: FLVar tag t -> FLExp tag t -> FLTopLevelVar tag
+data FLVarDecl (tag :: Type) where
+  FLVarDecl :: FLVar tag t -> FLExp tag t -> FLVarDecl tag
 
 data FLExp (tag :: Type) (t :: FLType) where
-  FLEValI   :: forall   tag      . Int -> FLExp tag 'FLTInt
-  FLEValB   :: forall   tag      . Int -> FLExp tag 'FLTBool
-  FLEVar    :: forall t tag      . FLVar tag t -> FLExp tag t
-  FLELambda :: forall t tag t1 t2. FLVar tag t -> FLExp tag t2  -> FLExp tag ('FLTLambda t1 t2)
-  FLEApp    :: forall t tag t1 t2. FLExp tag ('FLTLambda t1 t2) -> FLExp tag t1 -> FLExp tag t2
-  FLELet    :: forall t tag t1 t2. FLVar tag t1 -> FLExp tag t2 -> FLExp tag t2
+  FLEValI   :: forall tag      . Int -> FLExp tag 'FLTInt
+  FLEValB   :: forall tag      . Int -> FLExp tag 'FLTBool
+  FLEVar    :: forall tag t1   . FLVar tag t1 -> FLExp tag t1
+  FLELambda :: forall tag t1 t2. FLVar tag t1 -> FLExp tag t2  -> FLExp tag ('FLTLambda t1 t2)
+  FLEApp    :: forall tag t1 t2. FLExp tag ('FLTLambda t1 t2) -> FLExp tag t1 -> FLExp tag t2
+  FLELet    :: forall tag t2   . [FLVarDecl tag] -> FLExp tag t2 -> FLExp tag t2
 
 data FLProgram (tag :: Type) = FLProgram {
-      flpTopLevelVars :: M.Map tag (FLTopLevelVar tag)
+      flpTopLevelVars :: M.Map tag (FLVarDecl tag)
   }
--- src/SimpleLang/FromFuncLang.hs
+++ src/SimpleLang/FromFuncLang.hs
@@ -15,11 +15,13 @@ import Data.Text as T
 import Control.Category
 import Prelude hiding ((.), id)
 import Data.Map as M
+import Data.Foldable as F
+import Control.Monad
 
 type family FLTypeToSLType (t :: FLType) :: SLType where
     FLTypeToSLType 'FLTInt  = 'SLTInt
     FLTypeToSLType 'FLTBool = 'SLTInt
-    FLTypeToSLType ('FLTLambda t1 t2) = 'SLTStruct ('SLTFuncPtr (FLTypeToSLType t1 ': '[]) (FLTypeToSLType t2) ': (FLTypeToSLType t1 ': '[]))  
+    FLTypeToSLType ('FLTLambda t1 t2) = 'SLTStruct ('SLTFuncPtr (FLTypeToSLType t1 ': '[]) (FLTypeToSLType t2) ': (FLTypeToSLType t1 ': '[]))
     FLTypeToSLType ('FLTTuple ts)     = 'SLTStruct (MapFLTypeToSLType ts)
 
 type family MapFLTypeToSLType (ts :: [FLType]) :: [SLType] where
@@ -34,21 +36,78 @@ type family MapFLTypeToSLType (ts :: [FLType]) :: [SLType] where
 2. 不完全な適用はクロージャ（SLExp）に、完全な適用はSLEPushCallなりslmTailCallReturnなりにする
 -}
 
--- 識別子という言葉を英語に翻訳するとidentifierだが、
-data FLCUniqueIdentifier
+-- 識別子初出位置へのパス
+data FLCPath =
+      FLCPathTop
+    | FLCPathInLetDecl Text FLCPath
+    | FLCPathInLetBody      FLCPath
+    | FLCPathInAppR         FLCPath
+    | FLCPathInAppL         FLCPath
+    | FLCPathInLambda       FLCPath
+    deriving (Eq, Show, Ord)
+
+flcPathDeeperThan :: FLCPath -> FLCPath -> Bool
+flcPathDeeperThan p1 p2 =
+    case (p1, p2) of
+        (FLCPathTop, _) -> False
+        (_, FLCPathTop) -> True
+        (FLCPathInLetDecl _ p1', FLCPathInLetDecl _ p2') -> flcPathDeeperThan p1' p2'
+        (FLCPathInLetBody p1', FLCPathInLetBody p2')     -> flcPathDeeperThan p1' p2'
+        (FLCPathInAppR p1', FLCPathInAppR p2')           -> flcPathDeeperThan p1' p2'
+        (FLCPathInAppL p1', FLCPathInAppL p2')           -> flcPathDeeperThan p1' p2'
+        (FLCPathInLambda p1', FLCPathInLambda p2')       -> flcPathDeeperThan p1' p2'
+        _ -> False
+
+data FLCUniqueIdentifier = FLCUniqueIdentifier {
+      flcuiPath :: FLCPath
+    , flcuiName :: Text
+    } deriving (Eq, Show, Ord)
 
 -- 0+1
-flcPreprocess :: FLProgram Text -> FLProgram FLCUniqueIdentifier
-flcPreprocess = flcRenameIdentifier >>> flcLiftLambda
+flcPreprocess :: FLProgram Text -> Either Text (FLProgram FLCUniqueIdentifier)
+flcPreprocess = flcRenameIdentifier >>> fmap flcLiftLambda
 
 -- 0
-flcRenameIdentifier :: FLProgram Text -> FLProgram FLCUniqueIdentifier
-flcRenameIdentifier = undefined
+flcRenameIdentifier :: FLProgram Text -> Either Text (FLProgram FLCUniqueIdentifier)
+flcRenameIdentifier program =
+    let globalDict :: M.Map Text FLCUniqueIdentifier
+        globalDict = (\(FLVarDecl (FLVar n) _) -> FLCUniqueIdentifier FLCPathTop n) <$> flpTopLevelVars program
+        
+        go :: FLExp Text t -> M.Map Text FLCUniqueIdentifier -> FLCPath -> Either Text (FLExp FLCUniqueIdentifier t)
+        go e dict path =
+            case e of
+                FLEValI i           -> Right $ FLEValI i
+                FLEValB b           -> Right $ FLEValB b
+                FLEVar (FLVar name) ->
+                    case M.lookup name dict of
+                        Just newName ->  Right $ FLEVar (FLVar newName)
+                        Nothing -> Left $ "Undefined variable: " <> name <> " at " <> T.pack (show path)
+                FLELambda (FLVar name) body ->
+                    let newpath = FLCPathInLambda path
+                        newdict = M.insert name (FLCUniqueIdentifier newpath name) dict
+                    in  FLELambda (FLVar (FLCUniqueIdentifier newpath name)) <$> go body newdict newpath
+                FLEApp f x ->
+                    let newF = go f dict (FLCPathInAppL path)
+                        newX = go x dict (FLCPathInAppR path)
+                    in FLEApp <$> newF <*> newX
+                FLELet vs body ->
+                    let newDict = F.foldl' (\d (FLVarDecl (FLVar n) _) -> M.insert n (FLCUniqueIdentifier (FLCPathInLetBody path) n) d) dict vs
+                        newDecl = mapM (\(FLVarDecl (FLVar n) expr) -> FLVarDecl (FLVar (FLCUniqueIdentifier path n)) <$> go expr newDict (FLCPathInLetDecl n path)) vs
+                        newBody = go body newDict (FLCPathInLetBody path)
+                    in FLELet <$> newDecl <*> newBody
+    in  M.map (\(FLVarDecl (FLVar t) decl) ->
+                  FLVarDecl (FLVar (FLCUniqueIdentifier FLCPathTop t))
+                    <$> go decl globalDict FLCPathTop
+              ) >>>
+        M.mapKeys (FLCUniqueIdentifier FLCPathTop) >>>
+        sequence >>>
+        fmap FLProgram
+        $ flpTopLevelVars program
 
 -- 1
 flcLiftLambda :: FLProgram FLCUniqueIdentifier -> FLProgram FLCUniqueIdentifier
 flcLiftLambda = undefined
 
 -- 2
-flcCompleExp :: FLExp FLCUniqueIdentifier t -> TypedSLExp (FLTypeToSLType t)
-flcCompleExp = undefined
+flcComipleExp :: FLExp FLCUniqueIdentifier t -> TypedSLExp (FLTypeToSLType t)
+flcComipleExp = undefined
