a4b4f2783e3b154dcd728a11a578c40f474dc0e5
boletales <30789006+boletales@users.noreply.github.com>
2023-12-29 22:37:07 +0900

    WIP: static check

--- src/SimpleLang/StaticCheck.hs
+++ src/SimpleLang/StaticCheck.hs
@@ -1,21 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module SimpleLang.StaticCheck where
 
 import SimpleLang.Def
 import Control.Monad
 import Control.Category
-import Prelude hiding ((.), id)
-import Data.Text as T
+import Data.Text as T hiding (
+    words
+  )
 import Data.Map as M
+import Data.Set as S
 import Control.Monad.State
 import Control.Monad.Except
+import Data.Foldable as F
+import Prelude hiding (
+    id
+  , (.)
+  , lookup
+  , foldl
+  , words
+  , exp
+  )
 
-slscCheck :: SLProgram -> Either Text ()
-slscCheck (SLProgram decls) = 
-  forM_ (M.assocs decls) (\(fname, fblock) ->do
-        let SLFuncBlock sig block = fblock
-        let SLFuncSignature name args ret = sig
-        if name /= fname
-          then throwError $ "function name mismatch"
-          else pure ()
-        
-    )
+
+data SLSCFlags = SLSCFlags {
+  } deriving (Show)
+{-
+slscCheck' :: SLSCFlags -> SLProgram -> Either Text ()
+slscCheck' flags decls = 
+  forM_ (M.assocs decls) (\(fname, fblock) -> do
+            let SLFuncBlock sig block = fblock
+            let SLFuncSignature name args rettype = sig
+            if name /= fname
+              then throwError $ "function name mismatch"
+              else pure ()
+            
+            let argset =
+                  S.fromList $ snd $ F.foldl (\(argpos, argexps) t -> (argpos + sltSizeOf t, SLEArg t argpos : argexps)) (0, []) args
+
+            _ <- (`evalState` S.empty) $ runExceptT
+              let go 
+
+
+            pure ()            
+        )
+-}
